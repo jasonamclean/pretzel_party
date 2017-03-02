344fcd1d4c50bcbd1fe5989fb49b0266e2cf82ee
Jason McLean <jasonamclean@hotmail.com>
2017-03-01 22:45:58 -0700

    Create initial view pages, controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'about', to: 'pages#about'
+
+  get 'order', to: 'pages#order'
+
+  root to: 'pages#home'
 end
